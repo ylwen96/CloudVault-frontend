1127810d44df138a857a056adaeecc8922af0cf1
Yuelin Wen <wenyuelinca@gmail.com>
2025-03-04 08:08:28 -0500

    add route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import { Route, Routes, Outlet, Navigate } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login"
+import SignUp from "./pages/signup/SignUp"
+// import Login from "./pages/login/Login"
+import Fragment from "./pages/fragment/Fragment"
+import NotFound from "./pages/notFound/NotFound"
+
 
 function App() {
+
+  // const PrivateRoutes = () => {
+  //   if (isUserSignedIn && user.idToken !== '') {
+  //     return <Outlet />
+  //   } else {
+  //     return < Navigate to="/login" />
+  //   }
+  // }
+
   return (
-    <div className="App">
-      123
-    </div>
-  );
+    <>
+      {/* <Navbar /> */}
+      <Routes>
+        {/* <Route element={<PrivateRoutes />}> */}
+        <Route>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/fragments/:id" element={<Fragment />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={< NotFound />} />
+      </Routes>
+    </>)
 }
 
 export default App;
---
+++ src/pages/confirmSignUpForm/ConfirmSignUpForm.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ConfirmSignUpForm extends Component {
+    render() {
+        return (
+            <div>
+                Confirm sign up
+            </div>
+        );
+    }
+}
+
+export default ConfirmSignUpForm;
