2ed78ba955c10450f75a57d64edbb0870bd4c57e
Yuelin Wen <wenyuelinca@gmail.com>
2025-03-04 08:50:46 -0500

    add redux

---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { isSignedIn, getSessionValues } from '../../util/signInHelper'
+import { defaultStorageExpirationDate } from '../../util/storageHelper';
+import { createSignInSession, cleanupOnSignOut } from '../../util/signInHelper';
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    user: getSessionValues(),
+    isUserSignedIn: isSignedIn()
+  },
+  reducers: {
+    setUserSignIn: (state, action) => {
+      const { idToken, accessToken, username } = action.payload;
+      createSignInSession(username, idToken, accessToken, defaultStorageExpirationDate);
+      state.user = { idToken, accessToken, username }
+      state.isUserSignedIn = true;
+    },
+    setUserSignOut: (state, action) => {
+      cleanupOnSignOut();
+      state.isUserSignedIn = false
+    }
+  }
+})
+
+// Action creators are generated for each case reducer function
+export const { setUserSignIn, setUserSignOut } = authSlice.actions
+
+export default authSlice.reducer
---
+++ src/util/signInHelper.js
@@ -0,0 +1,48 @@
+import { getUser } from './auth';
+import { readStorage, StorageTypes, createStorage, eraseStorage } from './storageHelper';
+
+const getSessionValues = () => {
+    const userId = readStorage(StorageTypes.ID_TOKEN_STORAGE);
+    if (!userId) {
+        return {
+            username: "",
+            idToken: "",
+            accessToken: "",
+        };
+    } else {
+        return {
+            username: readStorage(StorageTypes.USERNAME_STORAGE) || '',
+            idToken: readStorage(StorageTypes.ID_TOKEN_STORAGE) || '',
+            accessToken: readStorage(StorageTypes.ACCESS_TOKEN_STORAGE) || '',
+        };
+    }
+};
+
+const createSignInSession = (username, idToken, accessToken, days) => {
+    createStorage(StorageTypes.USERNAME_STORAGE, username, days);
+    createStorage(StorageTypes.ID_TOKEN_STORAGE, idToken, days);
+    createStorage(StorageTypes.ACCESS_TOKEN_STORAGE, accessToken, days);
+};
+
+const isSignedIn = async () => {
+    try {
+        const user = await getUser();
+        if (user !== null) {
+            return user.idToken === readStorage(StorageTypes.ID_TOKEN_STORAGE)
+        } else {
+            return false
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        return false
+    }
+};
+
+
+const cleanupOnSignOut = () => {
+    eraseStorage(StorageTypes.USERNAME_STORAGE);
+    eraseStorage(StorageTypes.ID_TOKEN_STORAGE);
+    eraseStorage(StorageTypes.ACCESS_TOKEN_STORAGE);
+};
+
+export { getSessionValues, createSignInSession, isSignedIn, cleanupOnSignOut }
